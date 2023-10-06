acda0fcff61a7f61e372ebb1a035ca54cb87f274
Navruzbek02Hamrayev <hamrayevnavruz12@gmail.com>
2023-10-06 18:46:42 -0400

    return the s.index(*) or False

--- Index08.py
+++ Index08.py
@@ -6,5 +6,10 @@ def main(s):
     Returns:
         int: answer
     """
-    return
+    if s[0]=="*" or s[1]=="*" or s[2]=="*" or s[3]=="*":
+        return s.index("*")
+    if s[0]!="*" and s[1]!="*" and s[2]!="*" and s[3]!="*":
+        return False
+print(main("2*44"))
+print(main("good"))
         
